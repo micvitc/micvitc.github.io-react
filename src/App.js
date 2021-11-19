629135ab82af32d79e49985403bfbc24d0feb9c4
Shreyansh-Agrawal <shreyansh.brbd@gmail.com>
2021-11-19 18:06:30 +0530

    navbar skeleton, no design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Screen from './component/Screen';
 
 function App() {
   return (
     <div className="App">
-     
+     <Screen />
     </div>
   );
 }
